bce4abbf175d95590da0a63728d1ea3e8c43f032
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2024-11-18 23:38:18 -0800

    add abridged commands options

--- Assets/Scripts/UI/Console.cs
+++ Assets/Scripts/UI/Console.cs
@@ -72,43 +72,43 @@ public class Console : MonoBehaviour
     private bool HandleCommand(string commandArg)
     {
         string formatted = commandArg.Trim().ToLower();
-        string[] parsedCommand = SplitCommand(formatted);
+        string[] parsed = SplitCommand(formatted);
 
-        if (parsedCommand.Length == 0)
+        if (parsed.Length == 0)
         {
             Debug.LogWarning("Command cannot be empty.");
             return false;
         }
 
-        if (parsedCommand.Length >= 3 && parsedCommand[0] == "player")
+        if (parsed.Length >= 3 && (parsed[0] == "player" || parsed[0] == "p"))
         {
-            if (parsedCommand[1] == "speed")
+            if (parsed[1] == "speed" || parsed[1] == "s")
             {
-                if (float.TryParse(parsedCommand[2], out float speed))
+                if (float.TryParse(parsed[2], out float speed))
                 {
                     playerController.MoveSpeed = speed;
                     return true;
                 }
                 else
                 {
-                    Debug.LogWarning($"Invalid value for player speed: {parsedCommand[2]}");
+                    Debug.LogWarning($"Invalid value for player speed: {parsed[2]}");
                     return false;
                 }
             }
-            else if (parsedCommand[1] == "jump")
+            else if (parsed[1] == "jump" || parsed[1] == "j")
             {
-                if (float.TryParse(parsedCommand[2], out float jumpForce))
+                if (float.TryParse(parsed[2], out float jumpForce))
                 {
                     playerController.JumpForce = jumpForce;
                     return true;
                 }
                 else
                 {
-                    Debug.LogWarning($"Invalid value for player jump: {parsedCommand[2]}");
+                    Debug.LogWarning($"Invalid value for player jump: {parsed[2]}");
                     return false;
                 }
             }
-            else if (parsedCommand[1] == "values" && parsedCommand[2] == "reset")
+            else if ((parsed[1] == "values" || parsed[1] == "v") && (parsed[2] == "reset" || parsed[2] == "r"))
             {
                 playerController.ResetValues();
                 return true;
