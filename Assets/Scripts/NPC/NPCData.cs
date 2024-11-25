606020b6cbf3aa52e48abfbcc24a2dd2c00033fa
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2024-11-25 14:15:56 -0800

    add small tweaks

---
+++ Assets/Scripts/NPC/NPCData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewNPCData", menuName = "ScriptableObjects/NPCData")]
+public class NPCData : ScriptableObject
+{
+    public string[] Dialogues;
+}
