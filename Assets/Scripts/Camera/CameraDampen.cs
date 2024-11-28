9e6d3ecfe0eb8a03a1c25ca670bf289419f70a1a
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2024-11-27 16:40:53 -0800

    add camera smoothing (better to smooth out player movement instead..?)

---
+++ Assets/Scripts/Camera/CameraDampen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraDampen : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.1f;
+    public float delay = 0.02f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 delayedPos;
+    private Vector3 dampedPos;
+
+    private float verticalOffset = 1f;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + new Vector3(0, verticalOffset, 0);
+
+        delayedPos = Vector3.Lerp(delayedPos, targetPosition, Time.deltaTime / delay);
+
+        transform.position = Vector3.SmoothDamp(transform.position, delayedPos, ref velocity, smoothTime);
+    }
+}
